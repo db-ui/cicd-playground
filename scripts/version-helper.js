ca56c2596c7af44dc0d7d55f20c9f09bbb824f90
Duncan Kolba <duncan.kolba@deutschebahn.com>
2022-07-25 15:47:26 +0200

    ci: 🎡 WIP package publishing in CI

---
+++ scripts/version-helper.js
@@ -0,0 +1,7 @@
+import { argv } from 'process';
+const [, , version] = argv;
+const [semver, prerelease] = version.split('-');
+if (prerelease) {
+	throw new Error('This seems to be a prerelease - will not publish');
+}
+console.log('Version is: ', version);
